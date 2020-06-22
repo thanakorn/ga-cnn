70e575f62c055a5bbe749ac658852d3beb07c0e8
thanakorn <thanakorn.panyapiang@outlook.com>
2020-06-22 18:07:27 +0100

    Remove dropout

--- model/simple_cnn.py
+++ model/simple_cnn.py
@@ -1,32 +1,26 @@
 import torch
 import torch.nn as nn
-from model.genetic_module import GeneticNetwork
+from model.genetic_network import GeneticNetwork
+from genetic_algorithm.chromosomes import *
 
 class SimpleCNN(GeneticNetwork):
-    def __init__(self):
-        super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(5,5))
-        self.fc1 = nn.Linear(32 * 12**2, 128)
-        self.fc2 = nn.Linear(128, 10)
-        
+    @classmethod
+    def genetic_schema(cls):
+        schema = {
+            'conv1': ConvChromosome(1, 32, 5, 1),
+            'fc1': LinearChromosome(4608, 128),
+            'fc2': LinearChromosome(128, 10)
+        }
+        return schema
+    
     @torch.no_grad()
     def forward(self, x):
         out = self.conv1(x)
         out = torch.relu(out)
         out = torch.max_pool2d(out, (2,2))
-        out = torch.dropout(out, 0.2)
         out = out.view(out.shape[0], -1)
         out = self.fc1(out)
         out = torch.relu(out)
         out = self.fc2(out)
         
         return out
-    
-    @classmethod
-    def genetic_schema(cls):
-        schema = {
-            'conv1': (1, 32, 5),
-            'fc1': (4608, 128),
-            'fc2': (128, 10)
-        }
-        return schema
