9346f4c9ad059edd358cdd728a303e949d5a6bb6
thanakorn <thanakorn.panyapiang@outlook.com>
2020-06-22 13:19:19 +0100

    Define chromosome schema

---
+++ genetic_algorithm/chromosomes.py
@@ -0,0 +1,15 @@
+from collections import namedtuple
+from typing import TypeVar, Generic, Dict
+
+ConvChromosome = namedtuple(
+    'ConvChromosome',
+    ('in_channels', 'out_channels', 'kernel_size', 'stride')
+)
+
+LinearChromosome = namedtuple(
+    'LinearChromosome',
+    ('in_features', 'out_features')
+)
+
+C = TypeVar('C', ConvChromosome, LinearChromosome)
+ChromosomeSchema = Dict[str, C]
